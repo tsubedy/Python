00222fda4fde4371fdb376894a6a313897cfbdea
tsubedy <tsubedy@gmail.com>
2021-07-13 16:55:35 -0700

    Election data analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,8 @@
 # Budget Analysis
-
 import os
 import csv
 
 # files to load and output
-
 data_file = os.path.join('.', 'Resources', 'election_data.csv')
 output_file = os.path.join('.', 'Analysis', 'election_data_analysis.txt')
 
@@ -14,71 +12,83 @@ total_votes = 0
 # List of Candidates to be elected (options)
 candidates = []
 
-# Candidate that was voted by each voters
-candidates_voted = {}
-
 # Name of the winning candidate and the number of votes received
-winning_candidate = ""
-winning_count = 0
+candidate_voted = ""
+
 
 # Read election_data.csv
 with open(data_file) as election_data:
     reader = csv.reader(election_data)
 
-# read the header row
+    # read the header row
     header = next(reader)
     print(f"Header: {header}")
 
     for row in reader:
         # counting total votes (each row is counted as a vote)
-        total_votes = total_votes + 1
 
         # getting candidate's name from each row (index 2)
-        candidate_name = row[2]
-        candidates.append(candidate_name)
+        candidate_voted = row[2]
+        candidates.append(candidate_voted)
+    # print(candidates)
+    def counting(items):
+        d = {}
+        for i in items:
+            if i in d:
+                d[i] = d[i] + 1
+            else:
+                d[i] = 1
+        return d
 
-    # def unique(list1):
-    #     # insert the list to the set
-    #     list_set = set(list1)
-    #     # convert the set to the list
-    #     unique_list = (list(list_set))
-    #     for x in unique_list:
-    #         print(x)
-    #
-    # unique(candidates)
-    #
-    # from collections import Counter
-    # z = Counter(candidates)
-    # print(z)
+    # Counting votes for each candidates
+    z = counting(candidates)
+    # print(f"Vote counts for each candidates:  {z} ")  # name of the candidates with number of votes received
 
-    # if the name of the candidate is not already in the list, then keep adding to the list
-    if candidate_name not in candidates:
-        candidates.append(candidate_name)
+    # List of Name of the candidates
+    for i in z:
+        l = (i, z[i])
+        print("The number of votes:", l)
+    a1 = list(z.keys())
+    # print(f"Candidates {a1} ")
 
-        # track the vote counter for each candidates
-        candidates_voted[candidate_name] = 0
-        candidates_voted[candidate_name] = candidates_voted[candidate_name] + 1
+
+    a = list(z.values())
+    # print(f"Count of Votes:  {a} ")  # List of number of votes received by each candidate
+
+    # Total votes counted
+    t = sum(a)
+
+
+
+
+    print(f"=======================")
+    print(f"Total votes casted {t}")
+
+    # Calculating percentages
+    percentage = []
+    for candidate, vote in z.items():
+        z[candidate] = (vote / t) * 100
+        percentage = z[candidate]
+
+        # print(percentage)
+
+        # print(f"votes received by {a[1]}:  {a[0]},  {percentage: .2f} ")
+        # print("Percentages of votes", percentage)
+
+    maximum = max(z, key=z.get)
+    winner = (maximum, z[maximum])
 
 with open(output_file, "w") as txt_file:
     election_results = (
         f"Election Results\n"
         f"=======================\n"
-        f"Total Votes: {total_votes}\n"
+        f"Total Votes: {t}\n"
+        # f"votes by candidates {z}\n"
+        f"Votes received by \n"
+        
+        f" {a1[0]}:  {a[0]}, {winner[1]:.2f}\n"
+        
+        f"The winner is : {winner[0]} who got {winner[1]:.2f}"
+
     )
-
-print(election_results)
-
-for winner in candidates_voted:
-    votes = candidates_voted.get(winner)
-    vote_percentage = float(votes) / float(total_votes) * 100
-
-    print(f"percentage {vote_percentage}")
-    print(f"Votes {votes}")
-
-    # finding the winning candidate and their votes
-    if votes > winning_count:
-        winning_count = votes
-        winning_candidate = winner
-
-    voter_output = f"{winner}: {vote_percentage: .3f}% ({votes})\n"
-    print(voter_output, end="")
+# print(election_results)
